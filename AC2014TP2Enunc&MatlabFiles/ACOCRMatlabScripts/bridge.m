4759c8b8cf8acbd283e399b9f49185ad93ba79e3
davidfpc <davidcardopinto@hotmail.com>
2014-10-06 17:03:09 +0100

    memoria parece estar done, está é com poucos testes(?)

--- AC2014TP2Enunc&MatlabFiles/ACOCRMatlabScripts/bridge.m
+++ AC2014TP2Enunc&MatlabFiles/ACOCRMatlabScripts/bridge.m
@@ -4,7 +4,7 @@ NUMBER_OF_SAMPLES_BY_CASE = 5;
 NUMBER_OF_CASES = 10;
 perfs = load('PerfectArial.mat');
 Perfect = perfs.Perfect;
-data_file = load('P.mat');
+data_file = load('PTrain.mat');
 P = data_file.P;
 %% Memória associativa
 %%Mem_Assoc()
@@ -14,21 +14,29 @@ for i=1:10,
    Perfect(:,i) = Perfect(:,1)*i ;
 end
 %}
+% adapt the Perfect matrix to the dataset
 T = zeros(256,TOTAL_OF_TEST_CASES);
 for j=1:NUMBER_OF_CASES,
     for i=1:NUMBER_OF_SAMPLES_BY_CASE,
         T(:,(NUMBER_OF_SAMPLES_BY_CASE*(j-1))+i) = Perfect(:,j); 
     end   
 end
-P2 = zeros(256,TOTAL_OF_TEST_CASES);
+% calculate the weight
 Wp = T(:,1:TOTAL_OF_TEST_CASES) * pinv(P(:,1:TOTAL_OF_TEST_CASES));
+
+%Load the training dataset
+data_file2 = load('P.mat');
+Ptest = data_file2.P;
+
+P2 = zeros(256,TOTAL_OF_TEST_CASES);
 
 for j=1:TOTAL_OF_TEST_CASES,
     %Analyse each case
-    P2(:,j) = Wp * P(:,j);
+    P2(:,j) = Wp * Ptest(:,j);
     %Print the results
     showim(P2(:,j));
     pause();
+    clf('reset')
 end
 
 
