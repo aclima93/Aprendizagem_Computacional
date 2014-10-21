8424c54ff87b6e10b42fbf9e7bba75d834cae658
aclima93 <tocalagarto93@gmail.com>
2014-10-21 01:56:44 +0100

    - houston, we have liftoff. -- now we only need JH's slides to ease our ever lasting pain and split the data set into a training and testing set

--- AC2014TP3Enunc&MatlabFiles/MatlabScripts/mlnn.m
+++ AC2014TP3Enunc&MatlabFiles/MatlabScripts/mlnn.m
@@ -2,28 +2,27 @@ function mlnn( num_neurons, target, train_function, train_set, test_set, filenam
 
 net = feedforwardnet(num_neurons, train_function);
 net = train(net, train_set, target);
+
+%plot and print classification
 network_outputs = net(test_set);
 plot(network_outputs)
-print(1, '-dpng', filename);
+print(1, '-dpng', strcat(filename, '__1'));
 close Figure 1;
 
+%create it in a MATLAB figure
 jframe = view(net);
-
-%# create it in a MATLAB figure
 hFig = figure('Menubar','none', 'Position',[100 100 565 166]);
 jpanel = get(jframe,'ContentPane');
 [~,h] = javacomponent(jpanel);
 set(h, 'units','normalized', 'position',[0 0 1 1]);
 
-%# close java window
+%close java window
 jframe.setVisible(false);
 jframe.dispose();
 
-%# print to file
+%print to file
 set(hFig, 'PaperPositionMode', 'auto');
-saveas(hFig, 'out.png');
-
-%# close figure
+saveas(hFig, strcat(filename, '__2'), 'png');
 close(hFig);
 
 %performance = perform(net, targets, network_outputs)
