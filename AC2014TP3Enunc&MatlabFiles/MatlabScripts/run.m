a5d1cd3a09bfe623d122e1b45cec8290d95a7e96
davidfpc <davidcardopinto@hotmail.com>
2014-10-27 15:26:25 +0000

    replacing run with run_acl

--- AC2014TP3Enunc&MatlabFiles/MatlabScripts/run.m
+++ AC2014TP3Enunc&MatlabFiles/MatlabScripts/run.m
@@ -1,7 +1,26 @@
-dataFile = load('92202.mat');
-dataTarget = dataFile.Trg;
-dataFeatures = dataFile.FeatVectSel;
-plot(dataTarget);
-axis([0,400000,-1,2]);
-figure;
-plot(dataFeatures(:,1));
+
+clc;
+clear all;
+
+imgdir = 'images/simulations/';
+data_id = '92202';
+data = load(strcat(data_id, '.mat'));
+target = data.Trg;
+input_set = data.FeatVectSel;
+
+% quandooo deixar de ser pergui�oso tiro este debug e tento emter o
+% original a dar. also, dividir o dataset em training e testing com v�rias
+% percentagens
+[train_set, target] = simplefit_dataset;
+test_set = train_set;
+
+num_neurons = 5;
+train_func = 'trainlm';
+train_size = size(train_set);
+test_size = size(test_set);
+train_size = train_size(1)*train_size(2);
+test_size = test_size(1)*test_size(2);
+
+filename = strcat(imgdir, data_id, '_', num2str(num_neurons), '_', train_func, '_', num2str(train_size), '_', num2str(test_size));
+mlnn(num_neurons, target, train_func, train_set, test_set, filename);
+
