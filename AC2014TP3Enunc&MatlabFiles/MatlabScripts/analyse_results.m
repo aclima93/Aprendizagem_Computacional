d9502e613a89e0aaa8d45ad93e97480228a74760
aclima93 <tocalagarto93@gmail.com>
2014-11-17 18:43:17 +0000

    - a ton of images of the network outputs and binary outputs... but i didn't finish...

--- AC2014TP3Enunc&MatlabFiles/MatlabScripts/analyse_results.m
+++ AC2014TP3Enunc&MatlabFiles/MatlabScripts/analyse_results.m
@@ -2,17 +2,16 @@ load('dataset/results.mat', 'results');
 
 [num_sim, num_fields] = size(results);
 
-counter = 1;
-
 %{
 1) = filename
 2) = performance
 3) = network_outputs
 4) = binary_results
 5) = results_data
- = [accuracy, specificity, sensitivity, t_p, t_n, f_p, f_n, 
-    binary_positives, binary_negatives, invalids, expected_positives,
-    expected_negatives]
+ = [accuracy, specificity, sensitivity, 
+    t_p, t_n, f_p, f_n, binary_positives, 
+    binary_negatives, invalids, 
+    expected_positives, expected_negatives]
 %}
 
 %% perform averages for each unique simulation
@@ -25,15 +24,12 @@ for i = 1:len_unique_sim
     performance_means(i) = mean( results{find( strcmp(results(:,1), unique_sim(i))==1 ), 2} );
 end
 
-figure(counter);
+figure('Visible', 'off');
 hold on;
 plot( performance_means);
 %plot( repmat([mean(performance_means)], len_unique_sim), 'r'); %average of the averages
 hold off;
-print(counter, '-dpng', 'images/results/performances');
-counter = counter + 1;
-close;
-
+saveas(gcf, 'images/results/performances', 'png');
 
 
 %% results data
@@ -41,9 +37,9 @@ len_results_data = length(cell2mat(results(1,5)));
 results_data_means = zeros(len_unique_sim, len_results_data+1);
 results_data_means(:, 1) = 1:len_unique_sim;
 for i = 1:len_unique_sim
-    for j = 1: len_results_data
-        temp = results{find( strcmp(results(:,1), unique_sim(i))==1 ), 5};
-        results_data_means(i, j+1) = mean(temp(:, j));
+    net_outputs = results{find( strcmp(results(:,1), unique_sim(i))==1 ), 5};
+    for j = 1: len_results_data    
+        results_data_means(i, j+1) = mean(net_outputs(:, j));
     end
 end    
 
@@ -51,14 +47,13 @@ data_name = {'accuracy', 'specificity', 'sensitivity', 't_p', 't_n', 'f_p', 'f_n
 for j = 2: len_results_data+1
     
     % save image of all results and 
-    figure(counter);
+    figure('Visible', 'off');
     hold on;
     plot( results_data_means(:, j) );
     plot( repmat([mean(results_data_means(:, j))], len_unique_sim), 'r'); %average of the averages
     hold off;
-    print(counter, '-dpng', strcat('images/results/', data_name{j-1}));
-    counter = counter + 1;
-    close;
+    saveas(gcf, strcat('images/results/', data_name{j-1}),'png');
+
     
     % get num_best best of each aspect and save their matrix
     num_best = 20;
@@ -75,7 +70,28 @@ for j = 2: len_results_data+1
 end
 
 
-%% network outputs and binary outputs 
+%% network outputs and binary outputs
+for idx = 1:num_sim
+
+    net_outputs = results{idx, 3};
+    binary_outputs = results{idx, 4};
+
+    figure('Visible', 'off');
+
+    subplot(2,2,1)
+    plot( net_outputs(1,:) );
+    subplot(2,2,2)
+    plot( net_outputs(2,:) );
+
+    subplot(2,2,3)
+    plot( binary_outputs(1,:) );
+    subplot(2,2,4)
+    plot( binary_outputs(2,:) );
+
+    saveas(gcf,strcat('images/simulations/', num2str(idx)),'png');
+
+end    
+
 
 
 %% clean environment
